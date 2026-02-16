94380260873b4e8728b514f0ed8bd2df5a4030ae
CodeNova <100618080+arofficial005@users.noreply.github.com>
2026-02-16 20:09:22 +0500

    Update main.py
    update the new feature

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from app.routes import router
 app = FastAPI(title="AI Task Automation API")
 
 app.include_router(router)
-
+# home path
 @app.get("/")
 def health_check():
     return {"status": "running"}
